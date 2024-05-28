95a0dc830297ea2b7d64e5496cc2b15dbb84de99
Patrick <patrick@pop-os.localdomain>
2024-05-28 16:14:10 -0700

    debugging current directory

--- lib/work_api/jobs/make_cred.ex
+++ lib/work_api/jobs/make_cred.ex
@@ -4,6 +4,7 @@ defmodule WorkApi.Jobs.MakeCred do
   @impl Oban.Worker
   def perform(%Oban.Job{args: args}) do
     password = args["password"]
+    Logger.info(%{"current dir" => File.cwd!()})
     {_, 0} = System.cmd("pwsh", ["./ps_scripts/make_cred.ps1", password])
     :ok
   end
