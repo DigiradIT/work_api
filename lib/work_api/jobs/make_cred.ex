d9e28da2030040116b3746e8512f31d8fd27a370
Patrick <patrick@pop-os.localdomain>
2024-05-28 16:09:40 -0700

    fixed typo

--- lib/work_api/jobs/make_cred.ex
+++ lib/work_api/jobs/make_cred.ex
@@ -4,7 +4,7 @@ defmodule WorkApi.Jobs.MakeCred do
   @impl Oban.Worker
   def perform(%Oban.Job{args: args}) do
     password = args["password"]
-    {_, 0} = System.cmd("pwsh", ["/.ps_scripts/make_cred.ps1", password])
+    {_, 0} = System.cmd("pwsh", ["./ps_scripts/make_cred.ps1", password])
     :ok
   end
 end
