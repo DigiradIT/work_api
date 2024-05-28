e0e3a6ac73b58d9ce8973294e2a51bf2507cab3a
Patrick <patrick@pop-os.localdomain>
2024-05-28 12:10:43 -0700

    formatting

--- lib/work_api/token.ex
+++ lib/work_api/token.ex
@@ -1,17 +1,41 @@
 defmodule WorkApi.Token do
+  alias WorkApi.Errors.{UnexpectedResponse, UnexpectedContent}
+
   @moduledoc """
-  Used for requesting and managing machine identity from Azure. 
+  Used for requesting and managing machine identity from Azure.
   """
   @url "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://management.azure.com/"
 
-  @spec fetch() :: Map | Exception.t()
-  def fetch() do
-    case Req.get(@url, headers: %{"metadata" => true}) do
-      {:ok, resp} ->
-        resp
+  @typedoc """
+  A token that can be used to authenticate against MSFT APIs.
+  """
+  @type access_token :: String.t()
 
-      {:error, e} ->
-        e
+  @spec fetch() :: {:ok, access_token()} | {:error, Exception.t()}
+  def fetch() do
+    with {:ok, resp} <- Req.get(@url, headers: %{"metadata" => true}),
+         {:ok, resp} <- check_status(resp),
+         {:ok, token} <- check_content(resp) do
+      {:ok, token}
+    end
+  end
+
+  @spec check_status(Req.Response.t()) :: {:ok, Req.Response.t()} | {:error, Exception.t()}
+  defp check_status(%Req.Response{} = resp) do
+    if resp.status == 200 do
+      {:ok, resp}
+    else
+      {:error,
+       %UnexpectedResponse{expected_code: 200, response_code: resp.status, response: resp}}
+    end
+  end
+
+  @spec check_content(Req.Resposne.t()) :: {:ok, access_token()} | {:error, Exception.t()}
+  defp check_content(%Req.Response{} = resp) do
+    if token = resp.body["access_token"] do
+      {:ok, token}
+    else
+      {:error, %UnexpectedContent{expected: "body with access token field", received: resp.body}}
     end
   end
 end
