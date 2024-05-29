ebf770a7c7dd80172d094e798845a804831db44d
Patrick <patrick@pop-os.localdomain>
2024-05-28 21:57:56 -0700

    remove logging

--- lib/work_api/jobs/make_cred.ex
+++ lib/work_api/jobs/make_cred.ex
@@ -1,11 +1,9 @@
 defmodule WorkApi.Jobs.MakeCred do
   use Oban.Worker, queue: :commands
-  require Logger
 
   @impl Oban.Worker
   def perform(%Oban.Job{args: args}) do
     password = args["password"]
-    Logger.info(%{"current dir" => File.cwd!()})
     script_path = Application.app_dir(:work_api, "priv/ps_scripts/make_cred.ps1")
     {_, 0} = System.cmd("pwsh", [script_path, password])
     :ok
